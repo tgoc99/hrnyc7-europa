11c11515113de57947644e916855bc512c5d87df
Joel Camacho <jcamacho127@gmail.com>
2017-04-28 11:13:59 -0400

    Skeleton routes

---
+++ server/routes/routes.js
@@ -0,0 +1,52 @@
+module.exports = function(app, express) {
+
+	//query params : ?:username = ''&token=''
+	app.get('/api/users', function(req, res, next) {
+
+		next();
+	});
+
+	//query params : ?:username = ''&token=''
+	//in body: (username: ''password: '')
+	app.post('/api/users', function(req, res, next) {
+
+		next();
+	});
+
+	//query params : ?:username = ''&token=''
+	//body: (fields: )
+	app.update('/api/users', function(req, res, next) {
+
+		next();
+	});
+
+	//query params : ?:username = ''&token=''
+	app.delete('/api/users', function(req, res, next) {
+
+		next();
+	});
+
+	//query params : ?companyname
+	app.get('/api/news', function(req, res, next) {
+
+		next();
+	});
+
+	//query params : ?companyname
+	app.get('/api/company', function(req, res, next) {
+
+		next();
+	});
+
+	//query params :?username=''&password=''
+	app.get('/api/auth', function(req, res, next) {
+
+		next();
+	});
+
+	//in req.body: (username: password: )
+	app.post('/api/auth', function(req, res, next) {
+
+		next();
+	});
+};
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var request = require('request');
 
+
 var app = express();
 var port = process.env.PORT || 8080;
 
@@ -15,8 +16,14 @@ var db = mongoose.connection;
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
+
 //serve up static files
 app.use(express.static(__dirname + '/../client'));
+
+
+//set up routes
+var routes = require('./routes/routes.js')(app, express);
+
 
 app.listen(port);
 console.log('server listening on port ' + port);
