df28c1454108088a989a426ad6aabb7f8c0aa860
Hack Reactor Student <student@hackreactor.com>
2017-04-28 14:28:18 -0400

    add - setBackgroundImg function to be used in cards

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,4 +1,8 @@
 angular.module('app.dashboard',['ngMaterial'])
   .controller('dashboardController', function dashboardController($scope){
     $scope.jobs = sampleData;
+
+    $scope.setBackgroundImg = function(job) {
+      return `background-image:url("${job.imgURL}")`;
+    }
   });
