90f00e3bc2d5c9af61c8fdf67d17532c70add875
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-04-28 00:03:03 -0400

    complete - initial version of cards

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,5 +1,4 @@
 angular.module('app.dashboard',['ngMaterial'])
   .controller('dashboardController', function dashboardController($scope){
     $scope.jobs = sampleData;
-    $scope.message = 'Hello, world!';
   });
