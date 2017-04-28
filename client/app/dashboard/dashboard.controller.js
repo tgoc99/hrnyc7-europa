221600ed13dfc0514a5e93b763f8c3b188c06755
Hack Reactor Student <student@hackreactor.com>
2017-04-28 17:10:15 -0400

    continue - working on the dashboard, parse dates complete

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -5,4 +5,12 @@ angular.module('app.dashboard',['ngMaterial'])
     $scope.setBackgroundImg = function(job) {
       return `background-image:url("${job.imgURL}")`;
     }
+
+    $scope.getDate = function(job) {
+      var dateStr = job.applicationDate;
+      var date = new Date(dateStr);
+      var dateFormat = moment(date).format("MMM Do YY");
+      var fromNow = moment(date).fromNow();
+      return `${dateFormat}  |  ${fromNow}`
+    }
   });
