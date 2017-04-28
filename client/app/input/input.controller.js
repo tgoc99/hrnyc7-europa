c6d762710f32e2af1c4f0dc81e3873049878ee4a
Tommy O'Connor <tgoc99@gmail.com>
2017-04-28 15:56:59 -0400

    input html and controller complete first iteration

--- client/app/input/input.controller.js
+++ client/app/input/input.controller.js
@@ -1,4 +1,33 @@
-angular.module('app.input',['ngMaterial'])
-.controller('inputController', function(){
-  
+angular.module('app.input', [
+  'ngMaterial',
+  'ngMessages'
+])
+.controller('inputController', function($scope, $http, $location) {
+  var today = new Date();
+  var nextStepDate = new Date();
+  nextStepDate.setDate(nextStepDate.getDate() + 14);
+
+  $scope.job = {
+    appDate: today,
+    companyName: '',
+    position: '',
+    salary: '',
+    contact: {name: '',
+              phone:'',
+              email:''},
+    nextStep: {name: 'Send follow-up E-mail',
+               comments:'(Two weeks from now)',
+               date:nextStepDate},
+    currentStatus: 'Application Sent',
+    comments: ''
+  };
+
+  $scope.submitJob = function(data){
+    console.log('here')
+    $http.post('/jobs', $scope.job)
+    .then(function(data){
+      $location.path('/dashboard')
+    })
+  }
+
 })
