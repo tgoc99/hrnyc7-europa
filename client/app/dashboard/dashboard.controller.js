2ba30a2a7785b044a6471cdd2e7fd6bdb2c8acab
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-05-04 01:48:46 -0400

    fix - minor sintax

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,4 +1,11 @@
-angular.module('app.dashboard',['ngMaterial', 'profileWidget', 'newsWidget', 'goalsWidget', 'tasksWidget', 'calendarWidget', 'jobWidget'])
+angular.module('app.dashboard',
+  ['ngMaterial',
+   'profileWidget',
+   'newsWidget',
+   'goalsWidget',
+   'tasksWidget',
+   'calendarWidget',
+   'jobWidget'])
   .controller('dashboardController', function dashboardController($scope){
     $scope.jobs = sampleData;
   });
