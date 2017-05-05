fcb5f2a147578019b3aa4edfbaf8e411c4d9da8c
huawillian <huawillian@gmail.com>
2017-05-05 18:25:09 -0400

    finished front page

--- client/app/layout/auth.controller.js
+++ client/app/layout/auth.controller.js
@@ -11,9 +11,16 @@ angular.module('app.auth', [
     password: '',
   }
 
+  $scope.getStarted = false;
+
   $scope.showSignUp = true;
+
   $scope.toggleAuth = () => {
     $scope.showSignUp = !$scope.showSignUp;
+  }
+
+  $scope.toggleGetStarted = () => {
+    $scope.getStarted = !$scope.getStarted;
   }
 
   // send to correct path based on sign in or sign up
