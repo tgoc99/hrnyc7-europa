afb20d1cf99514c0be1f784ad6f5b40dfe7be822
Joel Camacho <jcamacho127@gmail.com>
2017-05-04 20:01:29 -0400

    Dashboard controller

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,6 +1,23 @@
-angular.module('app.dashboard',['ngMaterial', 'profileWidget', 'newsWidget', 'newsWidget', 'goalsWidget', 'calendarWidget', 'jobWidget', 'tasksWidget'])
+angular.module('app.dashboard',['ngMaterial', 'profileWidget', 'newsWidget', 'newsWidget', 'goalsWidget', 'calendarWidget', 'jobWidget', 'tasksWidget', 'app.services'])
   .controller('dashboardController', function dashboardController($scope, Companies, User, Jobs, Tasks){
-    $scope.jobs = sampleData;
+    
+    $scope.getJobs = function() {
+
+      $scope.jobs;
+
+      Jobs.get()
+      .then(function(data) {
+        if(data.length === 0) {
+          $scope.jobs = [{company: 'Input an application to get started!'}]
+        } else {
+          $scope.jobs = data
+        }
+      })
+      .catch(function(err) {
+        console.log(err)
+      })
+    }
+    $scope.getJobs()  
 
     $scope.setBackgroundImg = function(job) {
       return `background-image:url("${job.imgURL}")`;
