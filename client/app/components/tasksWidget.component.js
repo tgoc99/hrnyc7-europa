bdb6ef599de281ebb5c930f62e2e85714749a8be
Arturo Ruvalcaba <a.ruvham@gmail.com>
2017-05-06 02:31:58 -0400

    improve - functionality on the profile, tasks and goals widget

--- client/app/components/tasksWidget.component.js
+++ client/app/components/tasksWidget.component.js
@@ -7,35 +7,74 @@ angular.
     `
     <md-card id="tasks-widget" class='widget'>
       <span class="md-headline">Your upcoming tasks </span>
+
+      <div class="input-container">
+        <input type="text" placeholder="Add a new task..." ng-model="inputValue"></input>
+        <md-button class="md-icon-button" ng-click="$ctrl.createNewTask(inputValue); inputValue = null">
+            <md-icon>add</md-icon>
+        </md-button>
+      </div>
+
       <md-content>
+
         <ul>
-          <md-checkbox ng-repeat="task in $ctrl.tasksList">{{task.description}}</md-checkbox>
+          <li ng-repeat="task in $ctrl.tasksList">
+            <md-checkbox>{{task.description}}</md-checkbox>
+          </li>
         </ul>
+
       </md-content>
     </md-card>
     `,
     controller: function() {
-      // sample data
-      this.tasksList =
-      [
-        {
-          description: 'Bring back that Mulan szechuan McNugget sauce',
-          // other fields not used for the moment
-          // dueDate
-          // dateCreated
-        },
-        {
-          description: 'Save the earth',
-        },
-        {
-          description: 'Finish Greenfield project',
-        },
-        {
-          description: 'Graduate from Hack Reactor',
-        },
-        {
-          description: 'Become a billionare',
-        },
-      ];
+
+      // uncomment this for API call
+      // this.user = User.getAllData();
+      // use this meanwhile
+      this.user = {
+        tasksList: [
+          // {
+          //   description: 'Bring back that Mulan szechuan McNugget sauce'
+          // },
+          // {
+          //   description: 'Save the earth'
+          // },
+          // {
+          //   description: 'Finish Greenfield project'
+          // },
+          // {
+          //   description: 'Graduate from Hack Reactor'
+          // },
+          // {
+          //   description: 'Become a billionare'
+          // },
+        ]
+      };
+
+      this.tasksList = this.user.tasksList;
+
+      // we are not worrying about dates... for now
+
+      this.createNewTask = function(description) {
+
+        if(description && description.length > 0) {
+          this.tasksList.push({
+            description: description,
+            dateCreated: new Date()
+          });
+        }
+
+        // update database
+      }
+
+      this.deleteTask = function(description) {
+
+        this.tasksList = this.tasksList.filter(function(task) {
+          return task.description !== description;
+        });
+
+
+        // update database
+      }
     }
   });
